229dec162bd08a26e5372da4439b9166127a969d
b4ddr34m3r <46490490+b4ddr34m3r@users.noreply.github.com>
2020-12-17 17:46:05 +0400

    Update server.ino

--- server/server.ino
+++ server/server.ino
@@ -2,11 +2,13 @@
 #include <ESP8266WiFi.h>
 #include <Ultrasonic.h>
 
-Ultrasonic ulsc1(2,3); // the first ultrasonic sensor
-Ultrasonic ulsc2(5,6); // the second ultrasonic sensor
+ // the first ultrasonic sensor pins
+Ultrasonic ulsc1(2,3); // trig, echo
+// the second ultrasonic sensor pins
+Ultrasonic ulsc2(5,6); // trig, echo
 
 ESP8266WebServer server(80);   
- 
+// variables will hold the distance data from Sensors.
 int dist1 = 0, dist2 = 0;
 const char* ssid = "communicate";    
 const char* pass = "123456789";
@@ -19,16 +21,20 @@ void setup() {
 
 void loop() {
   Serial.print("The IP address is: ");
+ // start WIFI Access Point
   Serial.println(WiFi.softAPIP());
   server.handleClient();
   
   delay(100);
-    
+   
+ // reading the distance 
    dist1 = ulsc1.read();
    dist2 = ulsc2.read();
+ // printing the distance to the serial monitor
   Serial.println(String(dist1)+ " This is the distance1.");
   Serial.println(String(dist2)+ " This is the distance2.");
   
+ // 
   server.on("/", HTTP_GET, handleData);
 }
 
