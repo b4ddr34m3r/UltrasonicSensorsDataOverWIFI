865f8663b81b24952ce9edfda669806337ed30dd
b4ddr34m3r <46490490+b4ddr34m3r@users.noreply.github.com>
2020-12-20 10:13:25 +0400

    Update TestUltrasonic.ino

--- GeneralAltrasonicTest/TestUltrasonic.ino
+++ GeneralAltrasonicTest/TestUltrasonic.ino
@@ -1,5 +1,7 @@
 #include <Ultrasonic>
 
+// The T in these variables means Trigger
+// and E in these varaibles means Echo
 int U1T = 1, U1E = 2;
 int U2T = 3, U2E = 4;
 
@@ -11,9 +13,11 @@ void setup(){
 
 
 void loop(){
+  // Reading distances in centimeters
   int distanceU1 = ultrasonic1.read();
   int distanceU2 = ultrasonic2.read();
-  
+
+  // printing out the distances to the  serial monitors
   Serial.print("Distance of Ultrasonic sensor 1: "); Serial.println(distanceU1);
   Serial.print("Distance of Ultrasonic sensor 2: "); Serial.println(distanceU2);
 }
