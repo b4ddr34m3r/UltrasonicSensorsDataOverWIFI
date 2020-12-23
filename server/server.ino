9ffe6fe3b1f1c609cc4bd74393957f77600f9e7b
b4ddr34m3r <46490490+b4ddr34m3r@users.noreply.github.com>
2020-12-23 21:53:18 +0400

    Update server.ino

--- server/server.ino
+++ server/server.ino
@@ -10,6 +10,7 @@ Ultrasonic ulsc2(5,6); // trig, echo
 ESP8266WebServer server(80);   
 // variables will hold the distance data from Sensors.
 int dist1 = 0, dist2 = 0;
+int state1 = 0, state2 = 0;
 const char* ssid = "communicate";    
 const char* pass = "123456789";
 
@@ -33,19 +34,23 @@ void loop() {
  // printing the distance to the serial monitor
   Serial.println(String(dist1)+ " This is the distance1.");
   Serial.println(String(dist2)+ " This is the distance2.");
-  
- // 
-  server.on("/", HTTP_GET, handleData);
-}
-
-void handleData(){
-   int state1, state2;
-   // state mean the pin mode (HIGH, LOW)
+ 
+ // state mean the pin mode (HIGH, LOW)
    if(dist1 <= 50) state1 = 1;
    else state1 = 0;
    
    if(dist2 <= 50) state2 = 1;
    else state2 = 0;
  
+ 
+ // print out the states according to the distance
+  Serial.println(String(state1)+ " This is the state1.");
+  Serial.println(String(state2)+ " This is the state2.");
+
+ 
+ server.on("/", HTTP_GET, handleData);
+}
+
+void handleData(){
    server.send(200, "text/plain", String(state1) + " " + String(state2));
   }
